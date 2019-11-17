d175beadd4862effcc4a7c085b23a755c5c2ef23
Amirouche <amirouche+dev@hyper.dev>
2019-11-17 11:51:28 +0100

    babelia/pool.scm: add it.

---
+++ babelia/pool.scm
@@ -0,0 +1,35 @@
+;; pool of workers that can be used to execute blocking operation in a
+;; fibers application.
+(define-module (babelia pool))
+
+(import (ice-9 threads))
+(import (srfi srfi-9))
+
+(import (fibers channels))
+
+
+(define %channel #f)
+
+(define (worker channel)
+  (let loop ((message (get-message channel)))
+    (let ((thunk (car message))
+          (return (cdr message)))
+      ;; execute thunk and send the returned value
+      (let ((out (thunk)))
+        (put-message return out)))
+    (loop (get-message channel))))
+
+(define-public (pool-init)
+  (if %channel
+      (error 'babelia "pool can not be initialized more than once")
+      (let ((channel (make-channel)))
+        (let loop ((index (- (current-processor-count) 1)))
+          (unless (zero? index)
+            (call-with-new-thread (lambda () (worker channel)))
+            (loop (- index 1))))
+        (set! %channel channel))))
+
+(define-public (pool-apply thunk)
+  (let ((return (make-channel)))
+    (put-message %channel (cons thunk return))
+    (get-message return)))
