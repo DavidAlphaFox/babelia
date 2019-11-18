9227319c25f48d27915120212aada6bb1e833e8f
Amirouche <amirouche+dev@hyper.dev>
2019-11-18 17:28:35 +0100

    babelia/okvs/counter: improve and add tests.

--- babelia/okvs/counter.scm
+++ babelia/okvs/counter.scm
@@ -1,25 +1,23 @@
 ;; counter
 ;;
-;; This is an OKVS abstraction that allows to count. The counter is
-;; split into multiple rows to reduce contention, hence transaction
-;; roll-back and retry. It rely on thread-index that is two bytes
-;; hence 65 536 differents values. Thus, it is possible for as much
-;; threads to concurrently increment a counter. The trade-off is that
-;; reading the count is slower.
+;; The counter is split into multiple rows to reduce contention, hence
+;; reduce the chanced of transaction roll-back and retry. It rely on
+;; thread-index that is two bytes hence 65 536 differents
+;; values. Thus, it is possible for as much threads to concurrently
+;; increment a counter when thread-index is chosen wisely. The
+;; trade-off is that reading the count is slower.
 ;;
-;; As of today, the counter is used in the full-text index to keep
-;; track of how often a stems and words appear. Hence, the contention
-;; happens at index time. Slow reads happens all the time.
+;; Note: FoundationDB has atomic operations. And sqlite nested
+;; transactions.
+;;
+;; The counter is used in the full-text index to keep track of how
+;; often stems and words appear. Hence, the contention happens at
+;; index time. Slow reads happens all the time.
 ;;
 ;; TODO: It will be a good idea to cache word and stem count on a per
 ;; query request
 ;;
-;; As of today, it is not possible to decrement a counter, because
-;; unindexing, deleting indexed items in the full-text index, is not
-;; supported.
-;;
-;; TODO: it is missing the total count, to be able to compute
-;; frequency.
+;; TODO: Implement counter-decrement
 (define-module (babelia okvs counter))
 
 (import (srfi srfi-9))
@@ -28,33 +26,45 @@
 (import (babelia thread))
 (import (babelia bytevector))
 (import (babelia generator))
+(import (babelia okvs engine))
 
 
 (define-record-type <counter>
-  (make-counter prefix engine)
+  (make-counter engine prefix)
   counter?
-  (prefix counter-prefix)
-  (engine counter-engine))
+  (engine counter-engine)
+  (prefix counter-prefix))
 
+(export make-counter)
 
-(define-public (counter prefix engine)
-  (make-counter (apply engine-pack engine prefix) engine))
-
-(define-public (counter-increment transaction counter ulid)
+(define-public (counter-increment transaction counter object)
+  "Increment count for OBJECT"
   (let* ((engine (counter-engine counter))
          (prefix (counter-prefix counter))
-         (key (apply bytevector-append prefix ulid (thread-index)))
-         (count (engine-ref transaction key)))
-    (engine-set! engine transaction key (if count (+ counter 1) 1))))
+         (key (engine-pack engine prefix object (thread-index)))
+         (count (and=> (engine-ref engine transaction key)
+                       (lambda (key) (car (engine-unpack engine key)))))
+         (new-count (engine-pack engine (if count (+ count 1) 1))))
+    (engine-set! engine transaction key new-count)))
 
-(define-public (counter-ref transaction counter ulid)
+(define-public (counter-ref transaction counter object)
   (let* ((engine (counter-engine counter))
-         (prefix (counter-prefix counter))
-         (subspace (apply bytevector-append prefix ulid))
-         (generator (engine-range-prefix engine transaction subspace)))
+         (prefix (engine-pack engine (counter-prefix counter) object))
+         (generator (engine-prefix-range engine transaction prefix)))
     (let loop ((count 0)
-               (increment (generator)))
-      (if (eof-object? increment)
+               (key+value (generator)))
+      (if (eof-object? key+value)
           count
-          (loop (+ count increment)
+          (loop (+ count (car (engine-unpack engine (cdr key+value))))
+                (generator))))))
+
+(define-public (counter-total transaction counter)
+  (let* ((engine (counter-engine counter))
+         (prefix (engine-pack engine (counter-prefix counter)))
+         (generator (engine-prefix-range engine transaction prefix)))
+    (let loop ((count 0)
+               (key+value (generator)))
+      (if (eof-object? key+value)
+          count
+          (loop (+ count (car (engine-unpack engine (cdr key+value))))
                 (generator))))))
---
+++ babelia/okvs/counter/tests.scm
@@ -0,0 +1,60 @@
+(define-module (babelia okvs counter tests))
+
+(import (babelia testing))
+(import (babelia generator))
+(import (babelia okvs counter))
+(import (babelia okvs engine))
+(import (babelia okvs wiredtiger))
+
+
+(define engine (make-default-engine))
+
+(define counter (make-counter engine 'test-counter))
+
+(define-public test-00
+  (test  ;; ref unknown key
+   0
+   (with-directory "wt"
+     (let ((okvs (engine-open engine "wt")))
+       (let ((out (okvs-in-transaction okvs
+                    (lambda (transaction)
+                      (counter-ref transaction counter 'unknown)))))
+         (engine-close engine okvs)
+         out)))))
+
+(define-public test-01
+  (test  ;; set and ref
+   42
+   (with-directory "wt"
+     (let ((okvs (engine-open engine "wt")))
+       (let ((out (okvs-in-transaction okvs
+                    (lambda (transaction)
+                      (let loop ((index 42))
+                        (unless (zero? index)
+                          (counter-increment transaction counter 'magic)
+                          (loop (- index 1))))
+                      (counter-increment transaction counter 'one)
+                      (counter-increment transaction counter 'two)
+                      (counter-increment transaction counter 'two)
+                      (counter-ref transaction counter 'magic)))))
+         (engine-close engine okvs)
+         out)))))
+
+
+(define-public test-02
+  (test  ;; total
+   45
+   (with-directory "wt"
+     (let ((okvs (engine-open engine "wt")))
+       (let ((out (okvs-in-transaction okvs
+                    (lambda (transaction)
+                      (let loop ((index 42))
+                        (unless (zero? index)
+                          (counter-increment transaction counter 'magic)
+                          (loop (- index 1))))
+                      (counter-increment transaction counter 'one)
+                      (counter-increment transaction counter 'two)
+                      (counter-increment transaction counter 'two)
+                      (counter-total transaction counter)))))
+         (engine-close engine okvs)
+         out)))))
