f48483813c361a91e6bac6a6c17db0ab2496970a
Amirouche <amirouche+dev@hyper.dev>
2019-11-18 13:16:09 +0100

    emacs++

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,7 @@
 ;; The 'nil' configuration applies to all modes.
 ((scheme-mode . ((eval . (progn
                            ;; example
+                           (put 'okvs-in-transaction 'scheme-indent-function 1)
                            (put 'with-directory 'scheme-indent-function 1)
                            (put 'match 'scheme-indent-function 1)
                            (put 'with-connection 'scheme-indent-function 2))))))
