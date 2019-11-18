4c1bb85ad3b291e0b3054b763cb4e5b92dd62efb
Amirouche <amirouche+dev@hyper.dev>
2019-11-18 14:37:41 +0100

    babelia/okvs/mapping: add it.

---
+++ babelia/okvs/mapping.scm
@@ -0,0 +1,32 @@
+(define-module (babelia okvs mapping))
+
+(import (srfi srfi-9))
+(import (babelia generator))
+(import (babelia okvs engine))
+
+
+(define-record-type <mapping>
+  (make-mapping engine prefix)
+  mapping?
+  (engine mapping-engine)
+  (prefix mapping-prefix))
+
+(export make-mapping)
+
+(define-public (mapping-set transaction mapping key value)
+  (let* ((engine (mapping-engine mapping))
+         (key* (engine-pack engine (mapping-prefix mapping) key))
+         (value* (engine-pack engine value)))
+    (engine-set! engine transaction key* value*)))
+
+(define-public (mapping-ref transaction mapping key)
+  (let* ((engine (mapping-engine mapping))
+         (key* (engine-pack engine (mapping-prefix mapping) key))
+         (value (engine-ref engine transaction key*)))
+    (and=> value (lambda (value) (car (engine-unpack engine value))))))
+
+(define-public (mapping-generator transaction mapping)
+  (let ((engine (mapping-engine mapping)))
+    (gmap (lambda (key+value) (cons (cadr (engine-unpack engine (car key+value)))
+                                    (car (engine-unpack engine (cdr key+value)))))
+          (engine-prefix-range engine transaction (engine-pack engine (mapping-prefix mapping))))))
---
+++ babelia/okvs/mapping/tests.scm
@@ -0,0 +1,49 @@
+(define-module (babelia okvs mapping tests))
+
+(import (babelia testing))
+(import (babelia generator))
+(import (babelia okvs mapping))
+(import (babelia okvs engine))
+(import (babelia okvs wiredtiger))
+
+
+(define engine (make-default-engine))
+
+(define mapping (make-mapping engine #vu8(13 37)))
+
+(define-public test-00
+  (test  ;; ref unknown key
+   #f
+   (with-directory "wt"
+     (let ((okvs (engine-open engine "wt")))
+       (let ((out (okvs-in-transaction okvs
+                    (lambda (transaction)
+                      (mapping-ref transaction mapping 'unknown)))))
+         (engine-close engine okvs)
+         out)))))
+
+(define-public test-01
+  (test  ;; set and ref
+   42
+   (with-directory "wt"
+     (let ((okvs (engine-open engine "wt")))
+       (let ((out (okvs-in-transaction okvs
+                    (lambda (transaction)
+                      (mapping-set transaction mapping 'magic 42)
+                      (mapping-ref transaction mapping 'magic)))))
+         (engine-close engine okvs)
+         out)))))
+
+(define-public test-02
+  (test  ;; generator
+   '((1 . one) (2 . two) (3 . three))
+   (with-directory "wt"
+     (let ((okvs (engine-open engine "wt")))
+       (let ((out (okvs-in-transaction okvs
+                    (lambda (transaction)
+                      (mapping-set transaction mapping 1 'one)
+                      (mapping-set transaction mapping 2 'two)
+                      (mapping-set transaction mapping 3 'three)
+                      (generator-map->list values (mapping-generator transaction mapping))))))
+         (engine-close engine okvs)
+         out)))))
