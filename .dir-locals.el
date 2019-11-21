a16a900e9a840025cb61972184cafde255414013
Amirouche <amirouche+dev@hyper.dev>
2019-11-21 16:20:50 +0100

    emacs++

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,6 +2,7 @@
 ((scheme-mode . ((eval . (progn
                            ;; example
                            (put 'okvs-in-transaction 'scheme-indent-function 1)
+                           (put 'engine-in-transaction 'scheme-indent-function 2)
                            (put 'with-directory 'scheme-indent-function 1)
                            (put 'match 'scheme-indent-function 1)
                            (put 'with-connection 'scheme-indent-function 2))))))
